c3f090440d00ac9cfd6fd93d7dee33ec6a1ed26e
Oguama <oguamaprosperity@gmail.com>
2025-01-07 06:10:58 +0200

    revert ui

--- app2.py
+++ app2.py
@@ -62,10 +62,8 @@ def train(data):
 style = "<style>h2 {text-align: center;}</style>"
 st.markdown(style, unsafe_allow_html=True) 
 logo_path = "logo_svg.svg"
-logo2_path = "logo2_svg.svg"
 st.columns(3)[1].image(logo_path, width=300)
 st.markdown('<div style="text-align: center;">River Level Prediction App</div>', unsafe_allow_html=True)
-st.columns(3)[1].image(logo2_path, width=150)
 st.write("")
 st.write("")
 st.write("")
