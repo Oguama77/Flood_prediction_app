4468b05c7e5ff61e412cdc0c9de83c3e44edc5fc
Oguama <oguamaprosperity@gmail.com>
2025-01-07 05:41:46 +0200

    Text alignment

--- app2.py
+++ app2.py
@@ -61,10 +61,10 @@ def train(data):
 
 style = "<style>h2 {text-align: center;}</style>"
 st.markdown(style, unsafe_allow_html=True) 
-#st.title("Merak Labs")
 logo_path = "logo_svg.svg"
 st.columns(3)[1].image(logo_path, width=700)
-st.columns(3)[1].write("River Level Prediction App")
+#st.columns(3)[1].write("River Level Prediction App")
+st.markdown('<div style="text-align: center;">River Level Prediction App</div>', unsafe_allow_html=True)
 st.write("")
 st.write("")
 st.write("")
