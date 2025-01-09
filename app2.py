980b0ebd15b0d4f1d45e97750286e1d2971fa6c6
Oguama <oguamaprosperity@gmail.com>
2025-01-09 03:20:39 +0200

    reset original

--- app2.py
+++ app2.py
@@ -66,20 +66,21 @@ def get_base64_of_bin_file(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img = get_base64_of_bin_file("new_background.png")
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/jpeg;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
 
-page_bg_img = '''
-<style>
-.stApp {
-background-image: url("data:image/png;base64,%s");
-background-size: cover;
-background-repeat: no-repeat;
-background-attachment: fixed;
-}
-</style>
-''' 
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
+set_png_as_page_bg("new_backgroung.png")
 
 logo_path = "logo_svg.svg"
 
